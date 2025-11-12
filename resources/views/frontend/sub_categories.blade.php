8d4d00c2fed0b0a420f6b43898b72fe229b19d0b
Fares Shawa <fares.shawa@gmail.com>
2025-11-12 10:04:22 +0300

    website-fixes

--- resources/views/frontend/sub_categories.blade.php
+++ resources/views/frontend/sub_categories.blade.php
@@ -72,7 +72,30 @@
 
             <!-- Results -->
             <div id="results" class="row">
-
+                @foreach ($stores as $store)
+                    <div class="news-block_two col-lg-4 col-md-6 col-sm-12 mt-3">
+                        <div class="news-block_two-inner wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
+                            <div class="news-block_two-image">
+                                <a href="{{ route('store', ['id' => $store['id']]) }}">
+                                    test
+                                    <img src="{{ $store['main_image_url'] }}" alt="{{ $store['name'] }}" />
+                                </a>
+                            </div>
+                            <div class="news-block_two-content text-center">
+                                <h4 class="news-block_two-title">
+                                    <a href="{{ route('store', ['id' => $store['id']]) }}">{{ $store['name'] }}</a>
+                                </h4>
+                                <h6 class="news-block_two-title" style="font-size:13px;">
+                                    جدة - {{ $store['district'] }}
+                                </h6>
+                                <a href="{{ route('store', ['id' => $store['id']]) }}"
+                                   style="display:inline-block; background-color:#F2B100; color:#fff; padding:10px 25px; border-radius:25px; text-decoration:none; margin-top:15px; font-weight:600;">
+                                   عــرض التفــاصيل
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </div>
