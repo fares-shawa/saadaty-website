b5a0ceff66dd60dcc4f05c4511d816866ae5c213
Fares Shawa <fares.shawa@gmail.com>
2025-11-12 10:00:26 +0300

    website-fixes

--- resources/views/frontend/sub_categories.blade.php
+++ resources/views/frontend/sub_categories.blade.php
@@ -77,7 +77,7 @@
                         <div class="news-block_two-inner wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
                             <div class="news-block_two-image">
                                 <a href="{{ route('store', ['id' => $store['id']]) }}">
-                                    <img src="{{ asset($store['main_image_url']) }}" alt="{{ $store['name'] }}" />
+                                    <img src="{{ $store['main_image_url'] }}" alt="{{ $store['name'] }}" />
                                 </a>
                             </div>
                             <div class="news-block_two-content text-center">
