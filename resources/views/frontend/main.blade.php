203449fa0bdafa024b6842574e3cbcb091696609
Fares Shawa <fares.shawa@gmail.com>
2025-11-16 09:57:14 +0300

    Website Refactor and SEO

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -1,30 +1,14 @@
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <head>
-    <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0FHQNZRB5M"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-0FHQNZRB5M');
-</script>
     <meta charset="utf-8">
-    <title>سعادتــــي</title>
+    <title>سعادتي - حيث يلتقي الحلم بالواقع</title>
+    @include('layouts.seo')
     <link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700&amp;display=swap"
-            rel="stylesheet">
-    <link rel="shortcut icon" href="{{ asset('assets/images/logo-saadaty.png') }}" type="image/x-icon">
-    <link rel="icon" href="{{ asset('assets/images/logo-saadaty.png') }}" type="image/x-icon">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/css/custom.css') }}" rel="stylesheet">
     @yield('head')
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-    @yield('styles')
 </head>
 
 <body class="hidden-bar-wrapper">
@@ -42,20 +26,9 @@
     <script src="{{ asset('assets/js/jquery.js') }}"></script>
     <script src="{{ asset('assets/js/popper.min.js') }}"></script>
     <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('assets/js/appear.js') }}"></script>
-    <script src="{{ asset('assets/js/parallax.min.js') }}"></script>
-    <script src="{{ asset('assets/js/tilt.jquery.min.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.paroller.min.js') }}"></script>
-    <script src="{{ asset('assets/js/owl.js') }}"></script>
     <script src="{{ asset('assets/js/wow.js') }}"></script>
-    <script src="{{ asset('assets/js/element-in-view.js') }}"></script>
     <script src="{{ asset('assets/js/backtotop.js') }}"></script>
-    <script src="{{ asset('assets/js/odometer.js') }}"></script>
-    <script src="{{ asset('assets/js/parallax-scroll.js') }}"></script>
     <script src="{{ asset('assets/js/pagenav.js') }}"></script>
-    <script src="{{ asset('assets/js/jquery.countdown.js') }}"></script>
-    <script src="{{ asset('assets/js/magnific-popup.min.js') }}"></script>
-    <script src="{{ asset('assets/js/nav-tool.js') }}"></script>
     <script src="{{ asset('assets/js/jquery-ui.js') }}"></script>
     <script src="{{ asset('assets/js/script.js') }}"></script>
     @yield('scripts')
--- resources/views/layouts/seo.blade.php
+++ resources/views/layouts/seo.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Security -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>سعادتي - حيث يلتقي الحلم بالواقع </title>
-    <meta name="title" content="سعادتي - حيث يلتقي الحلم بالواقع">
+<meta name="title" content="سعادتي - حيث يلتقي الحلم بالواقع">
     <meta name="description" content="سعادتي ، يتحول زفافك من حلم إلى لوحة تفيض بالأناقة والبهجة نجمع لك كل ما تحتاجه في مكان واحد، لنحوّل كل تفصيلة إلى تجربة مترفة وسهلة ... سعادتي حيث يلتقي الحلم بالواقع ويُصبح الفرح أرقى مما تتصور">
     <meta name="keywords" content="سعادتي, قاعات افراح,أزياء,ورود وهدايا تذكارية,الفرق الشعبيه والفنانين,التصوير,صالون نسائي">
     <meta name="author" content="سعادتي">
-
-    <!-- Open Graph / Facebook -->
+     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:title" content="{{ config('app.name', 'Laravel') }}">
     <meta property="og:description" content="سعادتي ، يتحول زفافك من حلم إلى لوحة تفيض بالأناقة والبهجة نجمع لك كل ما تحتاجه في مكان واحد، لنحوّل كل تفصيلة إلى تجربة مترفة وسهلة ... سعادتي حيث يلتقي الحلم بالواقع ويُصبح الفرح أرقى مما تتصور">
@@ -34,34 +24,7 @@
     <link rel="apple-touch-icon" sizes="144x144" href="{{ asset('assets/images/logo.png') }}">
     <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('assets/images/logo.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/logo.png') }}">
-
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" href="{{ asset('assets/images/logo.png') }}"/>
-
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
-
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
-</html>
+    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700&amp;display=swap"
+            rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('assets/images/logo-saadaty.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('assets/images/logo-saadaty.png') }}" type="image/x-icon">
